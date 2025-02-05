df56bd7fa2320a9279329c6b45bfc96c95b2de39
Maxim Leyman <maximleyman@yandex.ru>
2025-02-05 23:14:28 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use eframe::egui::{self};
+use maliit_glib::input_method::InputMethod;
+
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_active(true)
+            .with_fullscreen(true),
+        renderer: eframe::Renderer::Glow,
+        window_builder: Some(Box::new(|builder| {
+            builder.with_active(true).with_fullscreen(true)
+        })),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Ok(Box::<MyApp>::default())
+        }),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+    input_method: Option<InputMethod>,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+            input_method: InputMethod::new().ok()
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ctx.set_pixels_per_point(3.0);
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                let text_edit = ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+                if text_edit.clicked() {
+                    self.input_method.as_ref().map(|im| im.show_input_method());
+                }
+                if text_edit.lost_focus() {
+                    self.input_method.as_ref().map(|im| im.hide_input_method());
+                }
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Increment").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+            ui.image(egui::include_image!("../assets/ferris.png"));
+        });
+    }
+}
